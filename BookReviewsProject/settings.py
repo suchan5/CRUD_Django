43a374b4640ddb48d6b73208d57a66b564cea604
suchan5 <suchan5@hotmail.com>
2020-08-05 21:04:20 +0000

    Added a master template

--- BookReviewsProject/settings.py
+++ BookReviewsProject/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'BookReviewsProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
